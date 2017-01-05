d1745cf12d6144ae5a4a09ab1f161deed109a631
Hayato oyanagi <oyana@oya.local>
2017-01-05 18:53:03 +0900

    allow English to faker

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Chatspase
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
+    Faker::Config.locale = :en
   end
 end
