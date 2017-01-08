fda09bf459f90bb2928f7fd1e998918e74f2c69a
Hayato oyanagi <oyana@oya.local>
2017-01-08 15:11:03 +0900

    create group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,20 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to :root, notice: 'グループが作成されました'
+    else
+      redirect_to new_group_path, alert: 'グループが作成されませんでした'
+    end
+    
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
