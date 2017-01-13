751c79d17bbab39fbfdc96a2814d7a2dcceb04c6
Hayato oyanagi <oyana@oya.local>
2017-01-13 19:37:12 +0900

    added accepts_nested_attributes_for

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,9 @@
 class Group < ApplicationRecord
-  has_many :group_users
+  has_many :group_users, index_errors: true, :inverse_of => :group
   has_many :messages
-  has_many :users, through: :group_user
+  has_many :users, through: :group_users
 
-  validates :name, presence: true 
+  accepts_nested_attributes_for :group_users
+
+  validates :name, presence: true
 end
