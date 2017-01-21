da67908d3a586d5ef8bc921fd4a7047ea7df8cc7
Hayato oyanagi <oyana@oya.local>
2017-01-21 18:03:58 +0900

    remove  unnecessary code

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,8 @@
 class Group < ApplicationRecord
-  has_many :group_users, index_errors: true, :inverse_of => :group
+  has_many :group_users
   has_many :messages
   has_many :users, through: :group_users
 
-  accepts_nested_attributes_for :group_users
 
   validates :name, presence: true
 end
