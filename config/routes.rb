b68447a52a8fbf88269544a35e37ddab3fad734a
Hayato oyanagi <oyana@oya.local>
2017-02-04 17:07:35 +0900

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update, :index] do
+    resources :messages, only: [:index, :delete, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "home#index"
+  root "groups#index"
 end
