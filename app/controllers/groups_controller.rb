c88689d6d82d71ce47935bc3e8be503199c88054
Hayato oyanagi <oyana@oya.local>
2017-02-08 21:30:07 +0900

    fixed controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,11 +9,13 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(group_params)
+    params[:group][:user_ids] << "#{current_user.id}"
+    @group = Group.new(strong_params)
     if @group.save
       redirect_to :root, notice: 'グループが作成されました'
     else
-      render action: :new, alert: 'グループが作成されませんでした'
+      flash.now[:alert] = 'グループが作成されませんでした'
+      render action: :new
     end
   end
 
@@ -22,25 +24,15 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-    if @group.update(group_params)
+    if @group.update(strong_params)
       redirect_to group_messages_path(@group), notice: 'グループが更新されました'
     else
-      render :action => 'edit', alert: 'グループが更新されませんでした'
+      flash.now[:alert] = 'グループが更新されませんでした'
+      render action: 'edit'
     end
   end
 
   private
-
-  def group_params
-
-    params_user << current_user.id unless params_user.include?("#{current_user.id}")
-    strong_params
-  end
-
-  #paramsのuser_id部分
-  def params_user
-    params[:group][:user_ids]
-  end
 
   def strong_params
     params.require(:group).permit(:name, { user_ids: [] })
