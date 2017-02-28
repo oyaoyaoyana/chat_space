9b4d6d580bfdb6cdeb6f2fed569d69171af65019
Hayato oyanagi <oyana@oya.local>
2017-02-28 20:27:22 +0900

    add js code for incremental search

---
+++ app/assets/javascripts/incremental_search.js
@@ -0,0 +1,45 @@
+function biuildUsers(user){
+  var html = $(`
+      <li class="user_result_list">
+        <span class="user_result_list--left">${user.name}</span>
+        <span id="user-add-btn" class="user_result_list--right">
+          <a href ="">追加
+            <input id="group_user_ids" name="group[user_ids]" type="hidden" value="${user.id}">
+          </a>
+        </span>
+      </li>
+    `);
+  return html
+};
+
+$(function(){
+  var preFunc = null
+  var preInput = ''
+  var input = ''
+  var ajaxSearch = function() {
+    $.ajax({
+      url: "search",
+      type: "GET",
+      data: ("keyword=" + input),
+      dataType: 'json',
+      timeout: 1000
+    })
+    .done(function(data){
+      $.each(data, function(){
+        user = biuildUsers(this)
+        $('#user_list_ul').append(user)
+      });
+    })
+    .fail(function(data){
+      alert('please reload');
+    });
+  };
+  $('#user-text-field').on('keyup', function() {
+    input = $.trim($(this).val());
+    if(preInput !== input){
+      clearTimeout(preFunc);
+      preFunc = setTimeout(ajaxSearch, 500);
+    }
+    preInput = input;
+  });
+});
