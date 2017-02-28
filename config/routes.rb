a77db762e9bc8fc14086fda495a74695f4d84f8e
Hayato oyanagi <oyana@oya.local>
2017-02-28 20:26:06 +0900

    add routing for search

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :groups, except: %i( delete ) do
     resources :messages, only: %i( index create )
+    get 'search', on: :collection
   end
   root "groups#index"
 end
