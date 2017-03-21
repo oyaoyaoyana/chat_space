f87d8a4ac985713c469499523f1eb5aa02c41e9f
Hayato oyanagi <oyana@oya.local>
2017-03-21 23:36:32 +0900

    image upload

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,19 @@
 class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
-  validates :body, presence: true
+  validates :body_or_image, presence: true
+
+   mount_uploader :image, ImageUploader
+
+
 
   def show_time
     self.created_at.strftime("%Y/%m/%d %T")
   end
+
+   private
+
+   def body_or_image
+     body.presence or image.presence
+   end
 end
