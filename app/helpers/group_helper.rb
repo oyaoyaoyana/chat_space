ae670e919f3d4d36abb6b5e17cb5b6025b81ffa5
Hayato oyanagi <oyana@oya.local>
2017-03-26 17:51:40 +0900

    refs #25 delete message

--- app/helpers/group_helper.rb
+++ app/helpers/group_helper.rb
@@ -1,8 +1,6 @@
 module GroupHelper
   def last_message(group)
-    # メッセージがあるかどうか？
     if group.messages.present?
-      # メッセージが有る場合、文字か画像か
       group.messages.last.body.present? ? "#{group.messages.last.body}" : "画像あり"
     else
       "メッセージはありません。"
